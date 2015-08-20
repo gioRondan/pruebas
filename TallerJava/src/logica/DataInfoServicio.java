3953e513e7d60d2346b38c5acbfd02de508c91bc
giovani.rondan <grondan@fing.edu.uy>
2015-08-20 17:01:02 -0300

    infoServicio

--- TallerJava/src/logica/DataInfoServicio.java
+++ TallerJava/src/logica/DataInfoServicio.java
@@ -4,11 +4,54 @@
  * and open the template in the editor.
  */
 package logica;
-
+import java.util.Set;
 /**
  *
  * @author giovani
  */
 public class DataInfoServicio {
+    private String nombre;
+    private String descripcion;
+    private String [] imagen;
+    private float precio;
+    private DataCiudad origen;
+    private DataCiudad destino;
+    private Set<DataCategoria> categorias;
+    public DataInfoServicio(String nombre, String descripcion, String [] imagen, float precio, DataCiudad origen, DataCiudad destino, Set<DataCategoria> categorias){
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+        this.imagen = imagen;
+        this.precio = precio;
+        this.origen = origen;
+        this.destino = destino;
+        this.categorias = categorias;
+    }
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public String[] getImagen() {
+        return imagen;
+    }
+
+    public float getPrecio() {
+        return precio;
+    }
+
+    public DataCiudad getOrigen() {
+        return origen;
+    }
+
+    public DataCiudad getDestino() {
+        return destino;
+    }
+
+    public Set<DataCategoria> getCategorias() {
+        return categorias;
+    }
     
 }
