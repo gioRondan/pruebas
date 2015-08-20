d6cd542493e0b54a785b75103fde32ca6a0b275d
giovani.rondan <grondan@fing.edu.uy>
2015-08-20 15:28:33 -0300

    altaServicio

--- TallerJava/src/logica/DataProveedor.java
+++ TallerJava/src/logica/DataProveedor.java
@@ -4,6 +4,8 @@
  * and open the template in the editor.
  */
 package logica;
+
+
 /**
  *
  * @author Juan
@@ -19,4 +21,36 @@ public class DataProveedor {
         this.apellido = apellido;
         this.empresa = empresa;
     }
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getEmpresa() {
+        return empresa;
+    }
+
+    public void setEmpresa(String empresa) {
+        this.empresa = empresa;
+    }
+    
 }
