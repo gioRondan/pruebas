861d218759cd5e0a9bd3a19dd24a58e1ce2a9999
giovani.rondan <grondan@fing.edu.uy>
2015-08-20 15:42:22 -0300

    altaServicio

--- TallerJava/src/logica/DataProveedor.java
+++ TallerJava/src/logica/DataProveedor.java
@@ -21,36 +21,22 @@ public class DataProveedor {
         this.apellido = apellido;
         this.empresa = empresa;
     }
+
     public String getNickname() {
         return nickname;
-    }
-
-    public void setNickname(String nickname) {
-        this.nickname = nickname;
     }
 
     public String getNombre() {
         return nombre;
     }
 
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
     public String getApellido() {
         return apellido;
-    }
-
-    public void setApellido(String apellido) {
-        this.apellido = apellido;
     }
 
     public String getEmpresa() {
         return empresa;
     }
 
-    public void setEmpresa(String empresa) {
-        this.empresa = empresa;
-    }
     
 }
