22e80937a5d89d9c308a0aef8817dc98e98d3d7d
giovani.rondan <grondan@fing.edu.uy>
2015-08-20 13:36:23 -0300

    manejadorProveedor

--- TallerJava/src/logica/ManejadorProveedor.java
+++ TallerJava/src/logica/ManejadorProveedor.java
@@ -5,18 +5,28 @@
  */
 
 package logica;
-import java.util.*;
+import java.util.Map;
 /**
  *
  * @author matias.heredia
  */
 public class ManejadorProveedor {
-
-
-
-
-
-
-
-
+    private Map<String,Proveedor> proveedores;
+    private static ManejadorProveedor instance = null;
+    private ManejadorProveedor(){}
+    
+    public static ManejadorProveedor getInstance(){
+        if(instance == null)
+            instance = new ManejadorProveedor();
+        return instance;
+    }
+    public Proveedor getProveedor(String nickname){
+        return proveedores.get(nickname);
+    }
+    public void addProveedor(Proveedor proveedor){
+        proveedores.put(proveedor.getNickname(), proveedor);
+    }
+    public void removeProvvedor(String nickname){
+        proveedores.remove(nickname);
+    }
 }
