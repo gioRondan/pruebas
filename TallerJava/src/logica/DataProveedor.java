954a436c368854fd40592eca0376590f969de7a2
giovani.rondan <grondan@fing.edu.uy>
2015-08-20 15:16:17 -0300

    altaServicio

--- TallerJava/src/logica/DataProveedor.java
+++ TallerJava/src/logica/DataProveedor.java
@@ -4,11 +4,19 @@
  * and open the template in the editor.
  */
 package logica;
-
 /**
  *
  * @author Juan
  */
 public class DataProveedor {
-    
+    private String nickname;
+    private String nombre;
+    private String apellido;
+    private String empresa;
+    public DataProveedor(String nombre, String apellido, String nickname, String empresa){
+        this.nickname = nickname;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.empresa = empresa;
+    }
 }
