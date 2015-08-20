229a52c5cec498eb7b2587663eaab94120fab1b5
giovani.rondan <grondan@fing.edu.uy>
2015-08-20 15:42:34 -0300

    actualizarServicio

--- TallerJava/src/logica/DataServicio.java
+++ TallerJava/src/logica/DataServicio.java
@@ -10,5 +10,25 @@ package logica;
  * @author Juan
  */
 public class DataServicio {
+    private String nombre;
+    private String descripcion;
+    private float precio;
+    public DataServicio(String nombre, String descripcion, float precio){
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+        this.precio = precio;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public float getPrecio() {
+        return precio;
+    }
     
 }
