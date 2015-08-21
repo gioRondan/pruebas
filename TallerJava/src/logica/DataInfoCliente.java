38786b998e96fc802d18fe2789f0f25e5f8f5479
diego.roman <diego.roman@pcunix69.fing.edu.uy>
2015-08-21 16:09:21 -0300

    DataInfoCliente

--- TallerJava/src/logica/DataInfoCliente.java
+++ TallerJava/src/logica/DataInfoCliente.java
@@ -5,10 +5,69 @@
  */
 package logica;
 
+import java.util.Date;
+import java.util.Set;
+
 /**
  *
- * @author giovani
+ * @author diego
  */
 public class DataInfoCliente {
+    private final String nickname;
+    private final String nombre;
+    private final String apellido;
+    private final String email;
+    private final Date fechaNac;
+    private final String imagen;    
+    private Set<DataReserva> reservas;
+    
+    //Constructores
+    public DataInfoCliente(){
+        this.nombre     = "";
+        this.nickname   = "";
+        this.apellido   = "";
+        this.email      = "";
+        this.fechaNac   = null;
+        this.imagen     = "";
+        this.reservas   = null;
+    }    
+     
+    public DataInfoCliente(String nickname, String nombre, String apellido, String email, Date fechaNac, String imagen, Set<DataReserva> reservas){
+        this.nombre     = nombre;
+        this.nickname   = nickname;
+        this.apellido   = apellido;
+        this.email      = email;
+        this.fechaNac   = fechaNac;
+        this.imagen     = imagen;
+    }         
+
+    //Getters
+    public String getNickname() {
+        return nickname;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+        
+    public String getApellido() {
+        return apellido;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public Date getFechaNac() {
+        return fechaNac;
+    }
+
+    public String getImagen() {
+        return imagen;
+    }
+    
+    public Set<DataReserva> getReservas(){
+        return reservas;
+    }
     
 }
