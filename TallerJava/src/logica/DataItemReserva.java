e2acc11d649c6b4d9c4f5893204b0c8f6b23f96e
diego.roman <diego.roman@pcunix69.fing.edu.uy>
2015-08-21 17:16:48 -0300

    DataItemReserva

--- TallerJava/src/logica/DataItemReserva.java
+++ TallerJava/src/logica/DataItemReserva.java
@@ -5,10 +5,50 @@
  */
 package logica;
 
+import java.util.Date;
+import java.util.Set;
+
 /**
  *
  * @author Juan
  */
 public class DataItemReserva {
+    private int cantidad; //cantidad de promociones o servicios reservados
+    private Date fechaInicio;
+    private Date fechaFin;
+    private String nombre; //nombre del servicio o promocion reservada
+    
+    //Constructores
+    public DataItemReserva(){
+        this.cantidad      = 0; 
+        this.fechaInicio   = new Date();
+        this.fechaFin      = new Date();
+        this.nombre        = nombre;     
+    }
+    
+    public DataItemReserva(int cantidad, Date fechaInicio, Date fechaFin, String nombre){
+        this.cantidad      = cantidad;
+        this.fechaInicio   = fechaInicio;
+        this.fechaFin      = fechaFin;
+        this.nombre        = nombre;
+    }    
+    
+    //Getters
+    public int getCantidad(){
+        return cantidad;
+    }
+        
+    public Date getFechaInicio(){
+        return fechaInicio;
+    }
+    
+    public Date getFechaFin(){
+        return fechaFin;
+    }
+            
+    public String getNombre(){
+        return nombre;
+    }
+    
     
 }
