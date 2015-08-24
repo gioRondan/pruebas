8390105f0d693dc2d9d5ba4f2f06ebbabf1bdafb
giovani.rondan <grondan@fing.edu.uy>
2015-08-24 16:35:49 -0300

    reservarservicio

--- TallerJava/src/logica/ControladorCliente.java
+++ TallerJava/src/logica/ControladorCliente.java
@@ -76,6 +76,7 @@ public class ControladorCliente implements IControladorCliente{
     }
     
     public DataInfoReserva verInfoReserva(int id){
+        
         return null;
     }
     
@@ -83,10 +84,5 @@ public class ControladorCliente implements IControladorCliente{
         ManejadorCliente mCl = ManejadorCliente.getInstance();
         return mCl.getCliente(nomCliente).getDataInfoCliente();
     }
-    
-    public void reservarPromocion(String proveedor, String promo, int cantidad, Date fechaIni, Date fechaFin){
-    };
-    public void reservarServicio(String proveedor, String promo, int cantidad, Date fechaIni, Date fechaFin){
-    };
 
 }
