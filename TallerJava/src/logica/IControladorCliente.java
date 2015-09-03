0d14e1c576c4923169d03250cab2a43da1a8c5e4
matias.heredia <matias.heredia@fing.edu.uy>
2015-09-03 13:29:07 -0300

    si son de prov distinto da error

--- TallerJava/src/logica/IControladorCliente.java
+++ TallerJava/src/logica/IControladorCliente.java
@@ -22,5 +22,5 @@ public interface IControladorCliente {
     public List<DataReserva> listarReservasSistema();
     public DataInfoReserva verInfoReserva(String nomCliente, int id);
     public DataInfoCliente verInfoCliente(String nomCliente);
-    public void realizarReserva(String proveedor,String cliente,Map<Integer, String> servicio, Map<Integer, String> promocion,Date fechaInicio,Date fechaFin );
+    public void realizarReserva(String proveedor,String cliente,Map<Integer, String> servicio, Map<Integer, String> promocion,Map<String,DataExpira> fechas );
 }
