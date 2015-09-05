e4f2f631ff3ba999643166a446c35d37bd7afd3e
GIO <grondan93@gmail.com>
2015-09-04 23:26:35 -0300

    test flujo

---
+++ TallerJava/test/logica/TestFlujo.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package logica;
+
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author giovani
+ */
+public class TestFlujo {
+    
+    public TestFlujo() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+    /**
+     * Test de flujo 1.
+     */
+    @Test
+    public void testFlujo1(){
+        
+    }
+    
+}
