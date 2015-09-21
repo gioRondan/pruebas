7bad1f67e7392cd2b7f9f92fbf18f3251b77acf2
giovani.rondan <grondan@fing.edu.uy>
2015-09-21 15:11:47 -0300

    controlador web

--- TallerJava/src/logica/ControladorWeb.java
+++ TallerJava/src/logica/ControladorWeb.java
@@ -6,10 +6,54 @@
 
 package logica;
 
+import java.util.Date;
+import java.util.Map;
+
 /**
  *
  * @author giovani.rondan
  */
-public class ControladorWeb {
+public class ControladorWeb implements IControladorWeb {
+//%%%%%%%%%%%%%%%%%%%%%%%%%%%%      MEMORIA     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    private Cliente cliente;
+    private Map<String, Integer> cantServicios;
+    private Map<String, Servicio> servicios;
+    private Map<String, Integer> cantPromociones;
+    private Map<String, Promocion> promociones;
+    private Map<String, DataExpira> fechasServicios;
+    private Map<String, DataExpira> fechasPromociones;
+    
+//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    
+    
+    @Override
+    public void iniciarSesion(String nickname, String password) throws Exception {
+        
+    }
+
+    @Override
+    public boolean ingresarNickEmail(String nickname, String password) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void registrarCliente(String nombre, String apellido, Date fechaNac, String password, String passwordVVerif) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void cerrarSesion() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void agregarServicioCarrito(String nickProveedor, String nomServicio) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void agregarPromocionCarrito(String nickProveedor, String nomPromocion) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
     
 }
--- TallerJava/src/logica/IControladorWeb.java
+++ TallerJava/src/logica/IControladorWeb.java
@@ -6,12 +6,18 @@
 
 package logica;
 
+import java.util.Date;
+
 /**
  *
  * @author giovani.rondan
  */
 public interface IControladorWeb {
-    public void iniciarSesion(String nickname, String password);
+    public void iniciarSesion(String nickname, String password) throws Exception;
     public boolean ingresarNickEmail(String nickname, String password);
-    public void registrarCliente(String nombre, String apellido);
+    public void registrarCliente(String nombre, String apellido, Date fechaNac, String password, String passwordVVerif);
+    public void cerrarSesion();
+    //hay que listar, capaz que usamos el listar de otro controlador
+    public void agregarServicioCarrito(String nickProveedor, String nomServicio);
+    public void agregarPromocionCarrito(String nickProveedor, String nomPromocion);
 }
