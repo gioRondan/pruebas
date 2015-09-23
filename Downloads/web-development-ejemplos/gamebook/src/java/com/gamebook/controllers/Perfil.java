e309df45b5094bfd43eb34bbd0eb43e87a01cdfd
giovani.rondan <grondan@fing.edu.uy>
2015-09-23 17:54:24 -0300

    probando

---
+++ Downloads/web-development-ejemplos/gamebook/src/java/com/gamebook/controllers/Perfil.java
@@ -0,0 +1,78 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.gamebook.controllers;
+
+import com.gamebook.exceptions.UsuarioNoEncontrado;
+import com.gamebook.model.EstadoSesion;
+import com.gamebook.model.Usuario;
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Igui
+ */
+public class Perfil extends HttpServlet {
+
+	/** 
+	 * Processes requests for both HTTP <code>GET</code> and <code>POST</code> methods.
+	 * @param request servlet request
+	 * @param response servlet response
+	 * @throws ServletException if a servlet-specific error occurs
+	 * @throws IOException if an I/O error occurs
+	 */
+	protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		try {
+			Usuario usr = Login.getUsuarioLogueado(request);
+			request.setAttribute("usuario", usr);
+			request.getRequestDispatcher("/WEB-INF/usuarios/perfil.jsp").forward(request, response);
+		} catch(UsuarioNoEncontrado ex){
+			// no existe el usuario, se trata como deslogueado
+			request.getSession().setAttribute("estado_sesion", EstadoSesion.NO_LOGIN);
+			request.getRequestDispatcher("/home").forward(request, response);
+		}
+	}
+
+	// <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+	/** 
+	 * Handles the HTTP <code>GET</code> method.
+	 * @param request servlet request
+	 * @param response servlet response
+	 * @throws ServletException if a servlet-specific error occurs
+	 * @throws IOException if an I/O error occurs
+	 */
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		processRequest(request, response);
+	}
+
+	/** 
+	 * Handles the HTTP <code>POST</code> method.
+	 * @param request servlet request
+	 * @param response servlet response
+	 * @throws ServletException if a servlet-specific error occurs
+	 * @throws IOException if an I/O error occurs
+	 */
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		processRequest(request, response);
+	}
+
+	/** 
+	 * Returns a short description of the servlet.
+	 * @return a String containing servlet description
+	 */
+	@Override
+	public String getServletInfo() {
+		return "Short description";
+	}// </editor-fold>
+}
