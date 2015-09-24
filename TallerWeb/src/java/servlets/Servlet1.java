d7fdd49ddd21e8c82bb5884d35d6825ffc494d3a
rodrigo.linares <rodrigo.linares@pcunix32.fing.edu.uy>
2015-09-24 19:40:08 -0300

    iniciar sesion pd soy gio con el usuario de rolinares que no esta haciendo nada

---
+++ TallerWeb/src/java/servlets/Servlet1.java
@@ -0,0 +1,104 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import logica.DataInfoCliente;
+import logica.Fabrica;
+import logica.IControladorCliente;
+
+/**
+ *
+ * @author rodrigo.linares
+ */
+@WebServlet(name = "Servlet1", urlPatterns = {"/Servlet1"})
+public class Servlet1 extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        Fabrica fab = Fabrica.getInstance();
+            IControladorCliente ICC = fab.getIControladorCliente();
+            String password = (String)request.getSession().getAttribute("txtpass");
+            String clave = (String) request.getSession().getAttribute("clave");
+            if(clave.equals("inicioSesion")){
+                String nick = (String) request.getParameter("txtnick");
+                String pass = (String) request.getParameter("txtpass");
+            try {
+//#############################     prueba      #####################################################
+                ICC.altaCliente(nick, nick, nick, nick, null, nick, pass);
+            } catch (Exception ex) {
+                Logger.getLogger(Servlet1.class.getName()).log(Level.SEVERE, null, ex);
+            }
+                
+//##################################################################################################
+                DataInfoCliente cliente = ICC.iniciarSesion(nick, pass);
+                if(cliente != null){
+                    request.getSession().setAttribute("dataCliente", cliente);
+                    request.getRequestDispatcher("/WEB-INF/perfil.jsp").forward(request, response);
+                }else{
+                    request.getRequestDispatcher("/index.jsp").forward(request, response);
+                }
+                
+            }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
--- TallerWeb/web/index.jsp
+++ TallerWeb/web/index.jsp
@@ -1,16 +1,24 @@
-<%@page contentType="text/html" pageEncoding="UTF-8" %>
+<%-- 
+    Document   : inicioSesion
+    Created on : Sep 23, 2015, 6:57:16 PM
+    Author     : giovani.rondan
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
 <!DOCTYPE html>
 <html>
 	<head>
-		<title>Help4Traveling</title>
-                <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        
+		<title>h4travel</title>
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 		
 	</head>
-       
 	<body>
-            <jsp:include page="/WEB-INF/template/header.jsp"/>
-            
-            
+              <form action="iniciarSesion" method="POST">
+                  <input name="txtnick" type="text" />
+                  <input name="txtpass" type="password"  />
+                  <input type="submit" value="Entrar"/>
+                  <% request.getSession().setAttribute("clave", "inicioSesion");%>
+                  
+              </form>
 	</body>
 </html>
