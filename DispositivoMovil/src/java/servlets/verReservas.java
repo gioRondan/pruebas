f93d0b47abc36ab992ef9203fbedefa157651120
giovani.rondan <grondan@fing.edu.uy>
2015-11-18 20:36:43 -0200

    ver reservas

--- DispositivoMovil/src/java/servlets/verReservas.java
+++ DispositivoMovil/src/java/servlets/verReservas.java
@@ -12,6 +12,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import servidor.DataProveedor;
 
 /**
  *
@@ -31,7 +32,11 @@ public class verReservas extends HttpServlet {
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        
+        servidor.PublicadorProveedorService service = new servidor.PublicadorProveedorService();
+        servidor.PublicadorProveedor port = service.getPublicadorProveedorPort();
+        DataProveedor proveedor = (DataProveedor) request.getSession().getAttribute("dataProveedor");
+        request.getSession().setAttribute("reservas", port.listarReservasXProveedor(proveedor.getNickname()));
+        request.getRequestDispatcher("/WEB-INF/verReservas.jsp").forward(request, response);
     }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
