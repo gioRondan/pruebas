5dfb03462ede47b11dbb1547ad679763012f335f
GIO <grondan93@gmail.com>
2015-11-21 13:05:28 -0200

    filtro servicios

---
+++ Administrador/src/RegistroAcceso/TopServicios.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package RegistroAcceso;
+
+import java.util.Map;
+import logica.DataServicio;
+
+/**
+ *
+ * @author giovani
+ */
+public class TopServicios {
+    private Map<Integer, DataServicio> servicios;
+
+    public TopServicios() {
+    }
+
+    public Map<Integer, DataServicio> getServicios() {
+        return servicios;
+    }
+    
+    
+}
